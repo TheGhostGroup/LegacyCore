8a2638d008bfff772766ef4d89b269218deefeca
TheGhost <theghostgroup@gmail.com>
2021-02-06 20:53:53 +1300

    DB/Gameobject: Rift to Telogrus spawn.

---
+++ sql/custom/world/2021_02_06_03_World.sql
@@ -0,0 +1,3 @@
+SET @OGUID := 780018;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnDifficulties`, `phaseUseFlags`, `PhaseId`, `PhaseGroup`, `terrainSwapMap`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `isActive`, `ScriptName`, `VerifiedBuild`) VALUES 
+(@OGUID+0, 279608, 0, 1519, 9171, '0', 0, 171, 0, -1, -8173.65, 776.347, 74.8599, 6.1951, 0, 0, -0.0440311, 0.99903, 120, 255, 1, 0, '', 26972);
