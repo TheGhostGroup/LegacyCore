0bc1215525b747eaaa1b9739a203c666e6725e97
TheGhost <theghostgroup@gmail.com>
2021-02-06 18:17:20 +1300

    DB/Creature: Lightforged Beacon Stormwind

---
+++ sql/custom/world/2021_02_06_02_World.sql
@@ -0,0 +1,3 @@
+SET @CGUID := 780021;
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnDifficulties`, `phaseUseFlags`, `PhaseId`, `PhaseGroup`, `terrainSwapMap`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `unit_flags2`, `unit_flags3`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES 
+(@CGUID+0, 130758, 0, 1519, 9171, '0', 0, 172, 0, -1, 0, 0, -8163.13, 777.229, 76.8477, 6.19509, 120, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 26972);
