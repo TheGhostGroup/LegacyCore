e97e54bf44b9e69ed2ee11ccee7ab21bbd9bf37d
TheGhost <theghostgroup@gmail.com>
2021-02-06 15:33:00 +1300

    DB/Gameobject: Add missing center piece spawn to Lion's Rest.

---
+++ sql/custom/world/2021_02_06_00_World.sql
@@ -0,0 +1,3 @@
+SET @OGUID := 780016;
+INSERT INTO `gameobject` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnDifficulties`, `phaseUseFlags`, `PhaseId`, `PhaseGroup`, `terrainSwapMap`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`, `isActive`, `ScriptName`, `VerifiedBuild`) VALUES 
+(@OGUID+0, 259008, 0, 1519, 8411, '0', 0, 0, 0, -1, -8736.3, 1044.13, 80.1375, 4.69494, 0, 0, -0.71325, 0.70091, 300, 255, 1, 0, '', 0);
