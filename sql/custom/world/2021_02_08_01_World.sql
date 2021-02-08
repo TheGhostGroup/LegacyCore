6806f428fe0c3243ac5581434caa17f4bfb572ac
TheGhost <theghostgroup@gmail.com>
2021-02-08 19:08:11 +1300

    DB/Creature: Ambassador Morrgard spawn.

---
+++ sql/custom/world/2021_02_08_01_World.sql
@@ -0,0 +1,3 @@
+SET @CGUID := 780022;
+INSERT INTO `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnDifficulties`, `phaseUseFlags`, `PhaseId`, `PhaseGroup`, `terrainSwapMap`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `unit_flags2`, `unit_flags3`, `dynamicflags`, `ScriptName`, `VerifiedBuild`) VALUES 
+(@CGUID+0, 133362, 0, 1519, 9171, '0', 0, 0, 0, -1, 0, 0, -8172.59, 801.071, 74.0504, 3.94803, 120, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 26972);
