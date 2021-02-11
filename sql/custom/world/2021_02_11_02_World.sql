2abf7ba9764c9bd959f282f6d0dff69133a7f461
TheGhost <theghostgroup@gmail.com>
2021-02-11 21:22:00 +1300

    DB/Creature: Feero Ironhand.

---
+++ sql/custom/world/2021_02_11_02_World.sql
@@ -0,0 +1,4 @@
+-- creature entry is not using smartai, skipped loading
+-- https://classic.wowhead.com/npc=4484/feero-ironhand
+
+UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 4484;
